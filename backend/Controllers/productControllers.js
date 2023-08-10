5d90f8d34954de65552fa749463bfd4ae4d39b33
Infinite-Null <ankit.kum.sha9933@gmail.com>
2023-08-10 22:20:56 +0530

    Created more routes in product"

--- backend/Controllers/productControllers.js
+++ backend/Controllers/productControllers.js
@@ -14,7 +14,46 @@ exports.createProduct=async(req,res,next)=>{
     })
 }
 
-exports.getAllProducts=(req,res)=>{
+
+//Admin route
+exports.updateProduct=async(req,res)=>{
+    Product.updateOne({_id:req.params.id},req.body).then((doc)=>{
+        res.status(200).json({
+            success:true,
+            details:doc
+        })
+    }).catch((e)=>{
+        console.log(e)
+    })
+}
+
+
+//Admin route
+exports.deleteProduct=async(req,res)=>{
+    Product.deleteOne({_id:req.params.id}).then((e)=>{
+        res.status(200).json({
+            success:true,
+            details:e
+        })
+    }).catch((e)=>{
+        console.log(e)
+    })
+}
+
+
+
+exports.getSingleProduct=(req,res,next)=>{
+    Product.findOne({_id:req.params.id}).then((doc)=>{
+        res.status(200).json({
+            success:true,
+            Product:doc
+        })
+    }).catch((e)=>{
+        console.log(e)
+    })
+}
+
+exports.getAllProducts=(_,res)=>{
    Product.find().then((e)=>{
     res.status(200).json({
     success:true,
--- backend/Routes/productRoute.js
+++ backend/Routes/productRoute.js
@@ -1,9 +1,13 @@
 const express = require("express")
-const { getAllProducts,createProduct} = require("../Controllers/productControllers")
+const { getAllProducts,createProduct,updateProduct,deleteProduct,getSingleProduct} = require("../Controllers/productControllers")
+
 
 const Router=express.Router()
 
 Router.route("/products").get(getAllProducts)
+Router.route("/products/:id").get(getSingleProduct)
 Router.route("/products/new").post(createProduct)
+Router.route("/products/:id").patch(updateProduct)
+Router.route("/products/:id").delete(deleteProduct)
 
 module.exports=Router
