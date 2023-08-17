3e7bd2146d7294c3e66174fbf6a6e73f22c5e063
Infinite-Null <ankit.kum.sha9933@gmail.com>
2023-08-17 22:49:43 +0530

    Worked on responsiveness

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,41 +7,57 @@ import {Categories} from "./Components/Categories/Categories";
 
 function App() {
     const x=[1,2,3,4,5]
+    const categories=[
+        {
+            link:"https://yourdesignstore.s3.amazonaws.com/uploads/yds/productImages/full/1686143600342516611518738868oversized-tee-website-1.png",
+            name:"Oversized"
+        },
+        {
+            link: "https://cdn.pixelbin.io/v2/black-bread-289bfa/woTKH5/wrkr/t.resize(h:1000,w:820)/data/Superdry/11oct2021/410294621021_4.jpg",
+            name:"Windcheater"
+        },
+        {
+            link:"https://5.imimg.com/data5/ECOM/Default/2023/6/313447685/CD/HO/QO/9422220/cargo-pants-men-solid-color-black-loose-casual-jogger-pocket-elastic-waist-ankle-length-977.jpg",
+            name:"Cargo-pants"
+        },
+        {
+            link:"https://cdn.thewirecutter.com/wp-content/media/2023/06/no-show-socks-2048px-1205.jpg?auto=webp&quality=75&width=1024",
+            name:"Socks"
+        },
+        {
+            link:"https://pyxis.nymag.com/v1/imgs/ba0/b4b/692b1c17930ef698abdc5549b51b2cf36e-bic-mens-gym-shorts.2x.rsocial.w600.jpg",
+            name:"Shorts"
+        },
+        {
+            link:"https://cdn.thewirecutter.com/wp-content/media/2021/01/whitetshirts-2048px-0297-2x1-1.jpg?auto=webp&quality=75&crop=1.91:1&width=1200",
+            name:"Relaxed Shirt"
+        }
+    ]
     return <>
         <NavBar/>
         <Banner/>
-        <Heading title={"Featured Products"}/>
+        <Heading title={"Featured"}/>
         <ProductLayout>
             {x.map((_,i)=> <ProductCard title="Classic Peace Lily" orignalPrice={"300"} link={"https://www.yourdesignstore.in/admin/uploads/654321/productImages/full/1632490432614dd3c096612Kid_OR.jpg"} discountPrice={"230"} key={i}/>
             )}
         </ProductLayout>
         <Heading title={"Categories"}/>
-        <Categories cat={[
-            {
-                link:"https://yourdesignstore.s3.amazonaws.com/uploads/yds/productImages/full/1686143600342516611518738868oversized-tee-website-1.png",
-                name:"Oversized"
-            },
-            {
-                link: "https://cdn.pixelbin.io/v2/black-bread-289bfa/woTKH5/wrkr/t.resize(h:1000,w:820)/data/Superdry/11oct2021/410294621021_4.jpg",
-                name:"Windcheater"
-            },
-            {
-                link:"https://5.imimg.com/data5/ECOM/Default/2023/6/313447685/CD/HO/QO/9422220/cargo-pants-men-solid-color-black-loose-casual-jogger-pocket-elastic-waist-ankle-length-977.jpg",
-                name:"Cargo-pants"
-            },
-            {
-                link:"https://cdn.thewirecutter.com/wp-content/media/2023/06/no-show-socks-2048px-1205.jpg?auto=webp&quality=75&width=1024",
-                name:"Socks"
-            },
-            {
-                link:"https://pyxis.nymag.com/v1/imgs/ba0/b4b/692b1c17930ef698abdc5549b51b2cf36e-bic-mens-gym-shorts.2x.rsocial.w600.jpg",
-                name:"Shorts"
-            },
-            {
-                link:"https://cdn.thewirecutter.com/wp-content/media/2021/01/whitetshirts-2048px-0297-2x1-1.jpg?auto=webp&quality=75&crop=1.91:1&width=1200",
-                name:"Relaxed Shirt"
-            }
-        ]}/>
+        <Categories cat={categories}/>
+        <Heading title={"Trending"}/>
+        <ProductLayout>
+            {x.map((_,i)=> <ProductCard title="Red Polo T-shirt" orignalPrice={"300"} link={"https://www.uniqlo.com/jp/ja/contents/feature/masterpiece/common_22fw/img/item_61_01.jpg?220211"} discountPrice={"230"} key={i}/>
+            )}
+        </ProductLayout>
+        <Heading title={"New Arrivals"}/>
+        <ProductLayout>
+            {x.map((_,i)=> <ProductCard title="Blue Print Shirt" orignalPrice={"300"} link={"https://imgmedia.lbb.in/media/2020/05/5eac0e7c833b2b7acdc583b2_1588334204878.jpg"} discountPrice={"230"} key={i}/>
+            )}
+        </ProductLayout>
+        <Heading title={"Best Sellers"}/>
+        <ProductLayout>
+            {x.map((_,i)=> <ProductCard title="White Relaxed Shirt" orignalPrice={"300"} link={"https://hips.hearstapps.com/hmg-prod/images/index-cuban-collar-1-64b6f686e40f3.jpg"} discountPrice={"230"} key={i}/>
+            )}
+        </ProductLayout>
     </>
 }
 
--- frontend/src/Components/EachProductCard/ProductCard.jsx
+++ frontend/src/Components/EachProductCard/ProductCard.jsx
@@ -4,29 +4,30 @@ import {FaCartPlus} from "react-icons/fa6";
 import {TbListDetails} from "react-icons/tb";
 export default function ProductCard(props) {
     return (
-        <><div className="card">
-            <div className="photo">
-                <img className="Imageproduct" src={props.link} alt="Not Found"/>
-            </div>
-            <div className="description">
-                <h2 className="ProductTitle">{props.title}</h2>
-                <h1 className="ProductPrice"><sapn className="ProductDupliPrice">{"₹"+props.orignalPrice}</sapn>{"₹"+props.discountPrice}</h1>
-                <div style={{
-                    display:"flex",
-                    alignItems:"stretch",
-                    justifyContent:"space-around",
-                    padding:"5px"
-                }}>
-                    <Button className="m-2 font-bold"  variant="ghost" startContent={<FaCartPlus/>}>Cart</Button>
-                    <Button className="m-2 font-bold"  variant="ghost" startContent={<TbListDetails/>}>Details</Button>
+            <div className="card">
+                <div className="photo">
+                    <img className="Imageproduct" src={props.link} alt="Not Found"/>
+                </div>
+                <div className="description">
+                    <h2 className="ProductTitle">{props.title}</h2>
+                    <h1 className="ProductPrice"><sapn className="ProductDupliPrice">{"₹"+props.orignalPrice}</sapn>{"₹"+props.discountPrice}</h1>
+                    <div style={{
+                        display:"flex",
+                        alignItems:"stretch",
+                        justifyContent:"space-around",
+                        padding:"5px"
+                    }}>
+                        <Button className="m-2 font-bold"  variant="ghost" startContent={<FaCartPlus/>}>Cart</Button>
+                        <Button className="m-2 font-bold"  variant="ghost" startContent={<TbListDetails/>}>Details</Button>
+                    </div>
                 </div>
             </div>
-        </div></>
     )
 }
 
 export function ProductLayout(props){
     return <div className="category">
-        {props.children}
-    </div>
+            {props.children}
+        </div>
+
 }
