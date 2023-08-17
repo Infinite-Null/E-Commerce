89bc74bc33c5559bd093c7974a5b44c51074134d
Infinite-Null <ankit.kum.sha9933@gmail.com>
2023-08-17 19:56:54 +0530

    Worked on banner and created card of product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import NavBar from "./Components/NavBar/Navbar";
 import Banner from "./Components/Banner/Banner";
+import ProductCard from "./Components/EachProductCard/ProductCard";
+import Heading from "./Components/Heading/Heading";
 
 function App() {
     return <>
         <NavBar/>
         <Banner/>
+        <Heading/>
+        <ProductCard title="Classic Peace Lily" orignalPrice={"300"} link={"https://www.yourdesignstore.in/admin/uploads/654321/productImages/full/1632490432614dd3c096612Kid_OR.jpg"} discountPrice={"230"}/>
     </>
 }
 
---
+++ frontend/src/Components/Heading/Heading.jsx
@@ -0,0 +1,7 @@
+import "./Heading.css"
+
+export default function Heading() {
+    return (
+        <h1 className="EachHeading"> Featured Products </h1>
+    )
+}
