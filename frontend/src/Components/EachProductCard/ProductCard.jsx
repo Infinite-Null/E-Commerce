b982e465e9b3cdc57458dbb738dce707364165e1
Infinite-Null <ankit.kum.sha9933@gmail.com>
2023-09-12 17:59:06 +0530

    Worked on product search  page

--- frontend/src/Components/EachProductCard/ProductCard.jsx
+++ frontend/src/Components/EachProductCard/ProductCard.jsx
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 export default function ProductCard(props) {
     const navigate = useNavigate()
     return (
+        <div className="group group-hover:bg-opacity-60 transition duration-500 relative bg-gray-50 flex justify-center items-center m-10">
             <div className="card">
                 <div className="photo">
                     <img className="Imageproduct" src={props.link} alt="Not Found"/>
@@ -12,25 +13,28 @@ export default function ProductCard(props) {
                 <div className="description">
                     <h2 className="ProductTitle">{props.title}</h2>
                     <h1 className="ProductPrice"><sapn className="ProductDupliPrice">{"₹"+props.orignalPrice}</sapn>{"₹"+props.discountPrice}</h1>
-                        <Button
-                            className="bg-gray-800 hover:bg-gray-950"
-                            style={{
-                                borderRadius:"0",
+                    <Button
+                        className="bg-gray-800 hover:bg-gray-950"
+                        style={{
+                            borderRadius:"0",
                             width:"300px",
                             height:"60px",color:"white"
                         }}  variant="flat" startContent={<TbListDetails/>} onPress={()=>{
-                            navigate("/product/"+props.id,{state:props.id})
-                        }}>Details</Button>
+                        navigate("/product/"+props.id,{state:props.id})
+                    }}>Details</Button>
                 </div>
             </div>
+        </div>
     )
 }
 
 export function ProductLayout(props){
     const width=(props.width!=null)?`${props.width}vw`:"99vw"
-    return<div className={`w-[100vw] flex items-center justify-center`}>
-        <div className={`category w-[100vw] md:w-[${width}]`}>
-            {props.children}
+    return<div className="mx-auto container">
+        <div className="flex flex-col">
+            <div className="mt-10 grid lg:grid-cols-3 items-center">
+                {props.children}
+            </div>
         </div>
     </div>
 
